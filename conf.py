efc998e78cee47978003a058d029b86846ce0b94
miyuru <miyuru@b8d5c20a-e689-4c1d-aa5f-1653b535320a>
2012-01-26 13:16:05 +0000

    Adding the configuration script of ScaleGraph.
    git-svn-id: svn://localhost:13690/proj/ScaleGraph/trunk@1436 b8d5c20a-e689-4c1d-aa5f-1653b535320a

---
+++ conf.py
@@ -0,0 +1,100 @@
+##########################################################
+##
+## Period  : Jan 2012
+##
+## program : conf.py
+##
+## Purpose : This script is used to set configuration
+##           information of the test environment.
+##           This is only for testing. Not for production.
+##           
+##
+#########################################################
+
+import sys
+import commands
+import socket
+
+def updateNPlaces():
+	while(True):
+		userchoice = raw_input('Enter number of places : ').lower().strip()
+
+                if (len(userchoice) == 0):
+                        break
+                try:
+                	val = int(userchoice)
+
+                        if val == 0:
+                           print('Error : Incorrect value. Please enter a non-zero integer.')
+                           continue
+                        break
+                except ValueError:
+                        print('Error : Incorrect value. Please enter a non-zero integer.')
+                        continue
+
+	if (len(userchoice)!=0):
+               configEntry = ''
+
+               for line in open('./Makefile'):
+                      if (line.find('X10_NPLACES=') != -1):
+                            line = 'X10_NPLACES=' + userchoice + '\r\n'
+       		      configEntry += line
+
+               open('./Makefile', 'w').write(str(configEntry))
+
+def main(item):
+	configEntry = ''
+	userchoice = ''
+
+	info = commands.getoutput('uname -a');
+	splitinfo = info.split(' ')
+
+	print('------- ScaleGraph Test Environment Configuration Wizard -------')
+
+	if((splitinfo[1][0:2] == 'sc') or (splitinfo[1][0:2] == 'st') or (splitinfo[1][0:2] == 'sd')):
+		print("Using Lab Cluster.")
+		userchoice = raw_input('Enter machine(s) to use : ').lower().strip()
+		if (len(userchoice)!=0):
+			machinesList = userchoice.split(',')
+
+			for record in machinesList:
+				configEntry += record + '\r\n'	
+			open('./machines.txt', 'w').write(str(configEntry))
+
+		updateNPlaces()
+	elif (splitinfo[1][0:2] == 't2'):
+		print("Using Tsubame 2.0")
+		tsubameStatus = commands.getoutput('t2stat -n').strip()
+
+                if (len(tsubameStatus) == 0):
+			print ('Error : No nodes have been allocated. Please allocate them using \'alloc\' command')
+			print ('Example : /data0/t2gsuzumuralab/apps/utils/bin/allocnode -q S -g t2gsuzumuralab -c 2 -n 4')		
+		else:
+			#print (tsubameStatus)
+			spinfo = tsubameStatus.split('\n')
+			line = spinfo[5].strip()
+			lastloc = line.rfind('/')
+			print line
+			print lastloc
+			counter = 0
+			prevcount = 0
+			hlist = []
+			while (lastloc != counter):
+				counter = line.find('/')
+				if counter == -1:
+					break
+				
+				hlist.append(line[prevcount:counter])
+				line = line[counter+1:]
+				prevcount = line.find('t')
+
+				if prevcount == -1:
+					break
+			configEntry = ''
+			for item in hlist:
+				configEntry += item + '\r\n'
+			open('./machines.txt', 'w').write(str(configEntry))
+			
+			updateNPlaces()
+main(sys.argv[1:])
+
