0784dacd536dd07c08deb0bad008b1a38b922c47
ogata <ogata@b8d5c20a-e689-4c1d-aa5f-1653b535320a>
2012-02-24 02:11:13 +0000

    Made dsygv function.
    git-svn-id: svn://localhost:13690/proj/ScaleGraph/trunk@1484 b8d5c20a-e689-4c1d-aa5f-1653b535320a

---
+++ src/org/scalegraph/clustering/LAPACKWrapper.cc
@@ -0,0 +1,28 @@
+#include <x10aux/config.h>
+#include "LAPACKWrapper.h"
+
+extern "C" {
+	int dsygv_(long int *itype, char *jobz, char *uplo, long int *n,
+			double *a, long int *lda, double *b, long int *ldb,
+			double *w, double *work, long int *lwork, long int *info);
+}
+
+/* TODO Think about using reference variable */
+int dsygv_wrap(long int itype, x10_char jobz, x10_char uplo, long int n,
+		double *a, long int lda, double *b, long int ldb,
+		double *w, double *work, long int lwork, long int *info){
+
+	return dsygv_(&itype, (char*)&jobz.v, (char*)&uplo.v, &n, a, &lda, b, &ldb, w, work, &lwork, info);
+}
+
+int sum(int *array, int n){
+	int i, s = 0;
+	for(i = 0; i < n; i++){
+		s += array[i];
+	}
+	return s;
+}
+
+void test_ptr(int *p){
+	*p = 10;
+}
