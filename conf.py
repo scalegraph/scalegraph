742bf598d015c135b635bf4f7fb355f6796a6106
miyuru <miyuru@b8d5c20a-e689-4c1d-aa5f-1653b535320a>
2012-03-18 03:23:52 +0000

    Updated the conf.py with support for explicit allocation of nodes on H Queue.
    git-svn-id: svn://localhost:13690/proj/ScaleGraph/trunk@1594 b8d5c20a-e689-4c1d-aa5f-1653b535320a

--- conf.py
+++ conf.py
@@ -44,9 +44,13 @@ def updateNPlaces():
                open('./Makefile', 'w').write(str(configEntry))
 
 def main(item):
-	
+	hostsFlag = False;
+	hostList = [];
 	if (len(item) > 0):
-		print 'Special mode'
+		if item[0] == '-hosts':
+			hostsFlag = True;
+			hostList = item[1].split(',')
+
 	
 	configEntry = ''
 	userchoice = ''
@@ -69,54 +73,56 @@ def main(item):
 		updateNPlaces()
 	elif (splitinfo[1][0:2] == 't2'):
 		print("Using Tsubame 2.0")
-		tsubameStatus = commands.getoutput('t2stat -n').strip()
-
-                if (len(tsubameStatus) == 0):
-			print ('Error : No nodes have been allocated. Please allocate them using \'alloc\' command')
-			print ('Example : /data0/t2gsuzumuralab/apps/utils/bin/allocnode -q S -g t2gsuzumuralab -c 2 -n 4')		
-		else:
-			#print (tsubameStatus)
-			spinfo = tsubameStatus.split('\n')
-			line = ''
-			for l in spinfo:
-				#if ( (l.find('+') != -1) and (l.find('*') != -1) and (l.find('/') != -1)):
-				if ( (l.find('*') != -1) and (l.find('/') != -1)):
-					line += l.strip()
-			#line = spinfo[len(spinfo) - 1].strip()
-			lastloc = line.rfind('/')
-			print line
-			print lastloc
-			counter = 0
-			prevcount = 0
-			hlist = []
-			while (lastloc != counter):
-				counter = line.find('/')
-				if counter == -1:
-					break
-				
-				hlist.append(line[prevcount:counter])
-				line = line[counter+1:]
-				prevcount = line.find('t')
-
-				if prevcount == -1:
-					break
-			configEntry = ''
-			for item in hlist:
-				configEntry += item + '\n'
-			open('./machines.txt', 'w').write(str(configEntry))
+		if not hostsFlag:
+			tsubameStatus = commands.getoutput('t2stat -n').strip()
 			
-			updateNPlaces()
-			configEntry = ''
-			for line in open('./Makefile'):
-				if (line.find('X10_HOME = ') != -1):
-							line = 'X10_HOME = /data0/t2gsuzumuralab/scalegraph/x10-runtimes/x10build/x10-2.2.2-fulloptimized-withgc-mpi/x10.dist\n'
-				if (line.find('APP_DIR=') != -1):
-							line = 'APP_DIR=' + os.path.dirname(os.path.abspath(__file__)) + '\n'
-				if (line.find('X10_NTHREADS=') != -1):
-							os.system(line)		
-							print(line)					
-	   			configEntry += line
+			if (len(tsubameStatus) == 0):
+				     print ('Error : No nodes have been allocated. Please allocate them using \'alloc\' command')
+				     print ('Example : /data0/t2gsuzumuralab/apps/utils/bin/allocnode -q S -g t2gsuzumuralab -c 2 -n 4')		
+			else:
+				#print (tsubameStatus)
+				spinfo = tsubameStatus.split('\n')
+				line = ''
+				for l in spinfo:
+					#if ( (l.find('+') != -1) and (l.find('*') != -1) and (l.find('/') != -1)):
+					if ( (l.find('*') != -1) and (l.find('/') != -1)):
+						line += l.strip()
+				#line = spinfo[len(spinfo) - 1].strip()
+				lastloc = line.rfind('/')
+				print line
+				print lastloc
+				counter = 0
+				prevcount = 0
 
-			open('./Makefile', 'w').write(str(configEntry))
+				while (lastloc != counter):
+					counter = line.find('/')
+					if counter == -1:
+						break
+					
+					hostList.append(line[prevcount:counter])
+					line = line[counter+1:]
+					prevcount = line.find('t')
+	
+					if prevcount == -1:
+						break
+			
+		configEntry = ''
+		for item in hostList:
+			configEntry += item + '\n'
+		open('./machines.txt', 'w').write(str(configEntry))
+		
+		updateNPlaces()
+		configEntry = ''
+		for line in open('./Makefile'):
+			if (line.find('X10_HOME = ') != -1):
+						line = 'X10_HOME = /data0/t2gsuzumuralab/scalegraph/x10-runtimes/x10build/x10-2.2.2-fulloptimized-withgc-mpi/x10.dist\n'
+			if (line.find('APP_DIR=') != -1):
+						line = 'APP_DIR=' + os.path.dirname(os.path.abspath(__file__)) + '\n'
+			if (line.find('X10_NTHREADS=') != -1):
+						os.system(line)		
+						print(line)					
+   			configEntry += line
+
+		open('./Makefile', 'w').write(str(configEntry))
 main(sys.argv[1:])
 
