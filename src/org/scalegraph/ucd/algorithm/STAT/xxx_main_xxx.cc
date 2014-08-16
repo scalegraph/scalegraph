0da57226cb7b929e44cbdf1cbeb3576b82abeb34
Vinod Madan <vinodmadaan@gmail.com>
2014-08-16 16:54:00 +0100

    Delete xxx_main_xxx.cc

--- src/org/scalegraph/ucd/algorithm/STAT/xxx_main_xxx.cc
+++
@@ -1,3 +0,0 @@
-#include <org/scalegraph/ucd/algorithm/STAT/STATS.h>
-#include <x10aux/bootstrap.h>
-extern "C" { int main(int ac, char **av) { return x10aux::template_main<org::scalegraph::ucd::algorithm::STAT::STATS>(ac,av); } }
